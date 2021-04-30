e8a37a2242b954115bb1a45b6114ddcbd90c6fb3
Sandeep Kumar <sk5545708@gmail.com>
2021-04-30 20:14:15 +0530

    added some file

---
+++ login.js
@@ -0,0 +1,12 @@
+// function checkpasswd(){
+//     var passwd = document.getElementById('passwd');
+//     var feedback = document.getElementById('feedback');
+
+//     if(passwd.Value.length < 8){
+//         feedback.textContent = 'Password must be 8 charactors or more';
+//     }
+//     else{
+//         feedback.textContent = '';
+//     }
+// }
+
